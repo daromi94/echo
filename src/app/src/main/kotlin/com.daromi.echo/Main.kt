4b3ab30fa1780eb54374ad01a8d3ddbc84320046
daromi94 <davromero07@gmail.com>
2026-02-13 21:54:16 -0500

    chore: simplify main entry point

--- src/app/src/main/kotlin/com.daromi.echo/Main.kt
+++ src/app/src/main/kotlin/com.daromi.echo/Main.kt
@@ -1,11 +1,5 @@
 package com.daromi.echo
 
-import com.daromi.echo.uds.Server
-
 fun main() {
-    val server = Server.create()
-
-    server.start()
-
-    Runtime.getRuntime().addShutdownHook(Thread { server.stop() })
+    println("Hello, World!")
 }
