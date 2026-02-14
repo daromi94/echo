572293b76e958b32b378922ea130904b6e5a01a4
daromi94 <davromero07@gmail.com>
2026-02-14 01:04:01 -0500

    refactor: simplify echo server handler

--- src/app/src/main/kotlin/com/daromi/echo/server/EchoServerHandler.kt
+++ src/app/src/main/kotlin/com/daromi/echo/server/EchoServerHandler.kt
@@ -1,8 +1,5 @@
 package com.daromi.echo.server
 
-import io.netty.buffer.ByteBuf
-import io.netty.buffer.Unpooled
-import io.netty.channel.ChannelFutureListener
 import io.netty.channel.ChannelHandler.Sharable
 import io.netty.channel.ChannelHandlerContext
 import io.netty.channel.ChannelInboundHandlerAdapter
@@ -13,15 +10,11 @@ class EchoServerHandler : ChannelInboundHandlerAdapter() {
         ctx: ChannelHandlerContext,
         msg: Any,
     ) {
-        val input = msg as ByteBuf
-
-        ctx.write(input)
+        ctx.write(msg)
     }
 
     override fun channelReadComplete(ctx: ChannelHandlerContext) {
-        val future = ctx.writeAndFlush(Unpooled.EMPTY_BUFFER)
-
-        future.addListener(ChannelFutureListener.CLOSE)
+        ctx.flush()
     }
 
     override fun exceptionCaught(
